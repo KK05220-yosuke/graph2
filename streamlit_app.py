ea899054146beef8f96382e1de69a031d3f8cbc5
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-10-26 06:24:19 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,54 +3,37 @@ import plotly.graph_objects as go
 import numpy as np
 
 # Streamlitのページ設定
-st.set_page_config(page_title="Complex 3D Function Visualizer")
+st.set_page_config(page_title="Classic 3D Function Visualizer")
 
 # ヘッダー
-st.title("Complex 3D Function Visualizer")
+st.title("Classic 3D Function Visualizer")
 st.caption("Created by ChatGPT")
 
 # ユーザーからの入力を受け取る
 n = st.slider("Choose the exponent (n)", min_value=1, max_value=10, value=2)
 x_min = st.number_input("Enter the minimum value of x", value=-10)
 x_max = st.number_input("Enter the maximum value of x", value=10)
-x_values_real = np.linspace(x_min, x_max, 100)  # 実数のxの値を生成
-x_values_complex = np.linspace(complex(x_min), complex(x_max), 100)  # 複素数のxの値を生成
+y_min = st.number_input("Enter the minimum value of y", value=-10)
+y_max = st.number_input("Enter the maximum value of y", value=10)
+x_values = np.linspace(x_min, x_max, 100)
+y_values = np.linspace(y_min, y_max, 100)
 
-# y = x^n の計算
-y_values_real = x_values_real ** n
-y_values_complex = x_values_complex ** n
+# 2Dグラフ（平面上の指数関数のプロット）
+x_mesh, y_mesh = np.meshgrid(x_values, y_values)
+z_mesh = (x_mesh + 1j * y_mesh) ** n
+surface = go.Surface(x=x_mesh, y=y_mesh, z=np.real(z_mesh), colorscale='Viridis')
 
 # 3Dプロットの作成
-fig = go.Figure()
-
-# 実数のxに対するプロット
-fig.add_trace(go.Scatter3d(
-    x=x_values_real,
-    y=y_values_real,
-    z=x_values_real,
-    mode='lines',
-    line=dict(color='blue', width=3),
-    name=f'y = x^{n} (Real)'
-))
-
-# 複素数のxに対するプロット
-fig.add_trace(go.Scatter3d(
-    x=x_values_complex.real,
-    y=y_values_complex.real,
-    z=x_values_complex.imag,
-    mode='lines',
-    line=dict(color='red', width=3),
-    name=f'y = x^{n} (Complex)'
-))
+fig = go.Figure(surface)
 
 # プロットの設定
 fig.update_layout(
     scene=dict(
         xaxis=dict(title='X'),
         yaxis=dict(title='Y'),
-        zaxis=dict(title='Imaginary Part of X'),
+        zaxis=dict(title='Z'),
     ),
-    title=f'3D Plot of y = x^{n}'
+    title=f'Classic 3D Plot of z = (x + iy)^n'
 )
 
 # プロットの表示
