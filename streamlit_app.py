1319640c135d66f04398cf1c6251ba948827f852
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-10-26 06:29:18 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import plotly.graph_objects as go
 import numpy as np
 
 # Streamlitのページ設定
-st.set_page_config(page_title="Classic 3D Function Visualizer")
+st.set_page_config(page_title="Complex 3D Function Visualizer")
 
 # ヘッダー
-st.title("Classic 3D Function Visualizer")
+st.title("Complex 3D Function Visualizer")
 st.caption("Created by ChatGPT")
 
 # ユーザーからの入力を受け取る
@@ -15,25 +15,37 @@ x_min = st.number_input("Enter the minimum value of x", value=-10)
 x_max = st.number_input("Enter the maximum value of x", value=10)
 y_min = st.number_input("Enter the minimum value of y", value=-10)
 y_max = st.number_input("Enter the maximum value of y", value=10)
-x_values = np.linspace(x_min, x_max, 100)
-y_values = np.linspace(y_min, y_max, 100)
 
-# 2Dグラフ（平面上の指数関数のプロット）
-x_mesh, y_mesh = np.meshgrid(x_values, y_values)
-z_mesh = (x_mesh + 1j * y_mesh) ** n
-surface = go.Surface(x=x_mesh, y=y_mesh, z=np.real(z_mesh), colorscale='Viridis')
+# 複素数のxの値を生成
+x_values_real = np.linspace(x_min, x_max, 100)
+y_values_real = np.linspace(y_min, y_max, 100)
+x_mesh, y_mesh = np.meshgrid(x_values_real, y_values_real)
+x_values_complex = x_mesh + 1j * y_mesh
+
+# y = x^n の計算
+y_values_complex = x_values_complex ** n
 
 # 3Dプロットの作成
-fig = go.Figure(surface)
+fig = go.Figure()
+
+# 複素数のxに対するプロット
+fig.add_trace(go.Surface(
+    x=x_values_complex.real,
+    y=x_values_complex.imag,
+    z=y_values_complex.real,
+    colorscale='Viridis',
+    cmin=y_min**n,
+    cmax=y_max**n
+))
 
 # プロットの設定
 fig.update_layout(
     scene=dict(
-        xaxis=dict(title='X'),
-        yaxis=dict(title='Y'),
-        zaxis=dict(title='Z'),
+        xaxis=dict(title='Real Part of X'),
+        yaxis=dict(title='Imaginary Part of X'),
+        zaxis=dict(title=f'Y = X^{n}'),
     ),
-    title=f'Classic 3D Plot of z = (x + iy)^n'
+    title=f'3D Plot of y = x^{n}'
 )
 
 # プロットの表示
