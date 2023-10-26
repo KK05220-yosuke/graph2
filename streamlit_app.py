914338c74c6b2d8e6e22542207a602aab6d8c618
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-10-26 07:05:38 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,19 @@ st.caption("Created by ChatGPT")
 
 # ユーザーからの入力を受け取る
 n = st.slider("Choose the exponent (n)", min_value=1, max_value=10, value=2)
-x_min = st.number_input("Enter the minimum value of x", value=-10)
-x_max = st.number_input("Enter the maximum value of x", value=10)
-y_min = st.number_input("Enter the minimum value of y", value=-10)
-y_max = st.number_input("Enter the maximum value of y", value=10)
 
-# 実数のxおよびyの値を生成
-x_values = np.linspace(x_min, x_max, 100)
-y_values = np.linspace(y_min, y_max, 100)
+# 実数または純虚数のxの値を生成
+x_type = st.radio("Select x type", ["Real", "Imaginary"])
+if x_type == "Real":
+    x_min = st.number_input("Enter the minimum value of x (Real)", value=-10)
+    x_max = st.number_input("Enter the maximum value of x (Real)", value=10)
+    x_values = np.linspace(x_min, x_max, 100)
+    y_values = np.linspace(0, 0, 100)  # 純虚数の場合はyはすべて0
+else:
+    y_min = st.number_input("Enter the minimum value of y (Imaginary)", value=-10)
+    y_max = st.number_input("Enter the maximum value of y (Imaginary)", value=10)
+    x_values = np.linspace(0, 0, 100)  # 実数の場合はxはすべて0
+    y_values = np.linspace(y_min, y_max, 100)
 
 # y = x^n の計算
 z_values = (x_values + 1j * y_values) ** n
@@ -26,22 +31,32 @@ z_values = (x_values + 1j * y_values) ** n
 # 3Dプロットの作成
 fig = go.Figure()
 
-# 実数および虚数のxに対するプロット
-fig.add_trace(go.Surface(
-    x=x_values,
-    y=y_values,
-    z=z_values.real,
-    colorscale='Viridis',
-    cmin=z_values.real.min(),
-    cmax=z_values.real.max()
-))
+# 実数または純虚数のxに対するプロット
+if x_type == "Real":
+    fig.add_trace(go.Scatter3d(
+        x=x_values,
+        y=y_values,
+        z=z_values.real,
+        mode='lines',
+        line=dict(color='blue', width=3),
+        name=f'y = x^{n} (Real)'
+    ))
+else:
+    fig.add_trace(go.Scatter3d(
+        x=x_values,
+        y=y_values,
+        z=z_values.real,
+        mode='lines',
+        line=dict(color='red', width=3),
+        name=f'y = x^{n} (Imaginary)'
+    ))
 
 # プロットの設定
 fig.update_layout(
     scene=dict(
         xaxis=dict(title='X'),
         yaxis=dict(title='Y'),
-        zaxis=dict(title='Real Part of y = x^{n}'),
+        zaxis=dict(title='Z'),
     ),
     title=f'3D Plot of y = x^{n}'
 )
