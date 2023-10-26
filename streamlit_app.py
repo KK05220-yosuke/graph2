fa8c37a944263f5f7ccc9596f043f078207be06c
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-10-26 06:51:49 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,65 +1,33 @@
 import streamlit as st
-import plotly.graph_objects as go
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Streamlitのページ設定
-st.set_page_config(page_title="Complex 3D Function Visualizer")
+# Streamlitアプリケーションのタイトルを設定
+st.title('3Dグラフ表示アプリ')
 
-# ヘッダー
-st.title("Complex 3D Function Visualizer")
-st.caption("Created by ChatGPT")
+# x, y, zの値を入力するスライダーを作成
+x_values = st.slider('X軸の範囲', -10.0, 10.0, 1.0)
+y_values = st.slider('Y軸の範囲', -10.0, 10.0, 1.0)
+z_values = st.slider('Z軸の範囲', -10.0, 10.0, 1.0)
 
-# ユーザーからの入力を受け取る
-n = st.slider("Choose the exponent (n)", min_value=1, max_value=10, value=2)
+# グラフを描画する関数
+def plot_3d_graph(x_range, y_range, z_range):
+    x = np.linspace(-x_range, x_range, 100)
+    y = np.linspace(-y_range, y_range, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = X**2 + Y**2 + z_range
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='viridis')
+    ax.set_xlabel('X軸')
+    ax.set_ylabel('Y軸')
+    ax.set_zlabel('Z軸')
+    ax.set_title('3D グラフ')
 
-# 実数または純虚数のxの値を生成
-x_type = st.radio("Select x type", ["Real", "Imaginary"])
-if x_type == "Real":
-    x_min = st.number_input("Enter the minimum value of x (Real)", value=-10)
-    x_max = st.number_input("Enter the maximum value of x (Real)", value=10)
-    x_values = np.linspace(x_min, x_max, 100)
-    y_values = np.linspace(0, 0, 100)  # 純虚数の場合はyはすべて0
-else:
-    y_min = st.number_input("Enter the minimum value of y (Imaginary)", value=-10)
-    y_max = st.number_input("Enter the maximum value of y (Imaginary)", value=10)
-    x_values = np.linspace(0, 0, 100)  # 実数の場合はxはすべて0
-    y_values = np.linspace(y_min, y_max, 100)
+    # グラフを表示
+    st.pyplot(fig)
 
-# y = x^n の計算
-z_values = (x_values + 1j * y_values) ** n
-
-# 3Dプロットの作成
-fig = go.Figure()
-
-# 実数または純虚数のxに対するプロット
-if x_type == "Real":
-    fig.add_trace(go.Scatter3d(
-        x=x_values,
-        y=y_values,
-        z=z_values.real,
-        mode='lines',
-        line=dict(color='blue', width=3),
-        name=f'y = x^{n} (Real)'
-    ))
-else:
-    fig.add_trace(go.Scatter3d(
-        x=x_values,
-        y=y_values,
-        z=z_values.real,
-        mode='lines',
-        line=dict(color='red', width=3),
-        name=f'y = x^{n} (Imaginary)'
-    ))
-
-# プロットの設定
-fig.update_layout(
-    scene=dict(
-        xaxis=dict(title='X'),
-        yaxis=dict(title='Y'),
-        zaxis=dict(title='Z'),
-    ),
-    title=f'3D Plot of y = x^{n}'
-)
-
-# プロットの表示
-st.plotly_chart(fig)
+# グラフを描画する関数を呼び出し
+plot_3d_graph(x_values, y_values, z_values)
