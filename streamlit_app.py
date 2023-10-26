fa384ec88e91345174edfdd3d72c97e059fafdd1
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-10-26 06:35:49 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,39 +11,52 @@ st.caption("Created by ChatGPT")
 
 # ユーザーからの入力を受け取る
 n = st.slider("Choose the exponent (n)", min_value=1, max_value=10, value=2)
-x_min = st.number_input("Enter the minimum value of x", value=-10)
-x_max = st.number_input("Enter the maximum value of x", value=10)
-y_min = st.number_input("Enter the minimum value of y", value=-10)
-y_max = st.number_input("Enter the maximum value of y", value=10)
 
-# 複素数のxの値を生成
-x_values_real = np.linspace(x_min, x_max, 100)
-y_values_real = np.linspace(y_min, y_max, 100)
-x_mesh, y_mesh = np.meshgrid(x_values_real, y_values_real)
-x_values_complex = x_mesh + 1j * y_mesh
+# 実数または純虚数のxの値を生成
+x_type = st.radio("Select x type", ["Real", "Imaginary"])
+if x_type == "Real":
+    x_min = st.number_input("Enter the minimum value of x (Real)", value=-10)
+    x_max = st.number_input("Enter the maximum value of x (Real)", value=10)
+    x_values = np.linspace(x_min, x_max, 100)
+    y_values = np.linspace(0, 0, 100)  # 純虚数の場合はyはすべて0
+else:
+    y_min = st.number_input("Enter the minimum value of y (Imaginary)", value=-10)
+    y_max = st.number_input("Enter the maximum value of y (Imaginary)", value=10)
+    x_values = np.linspace(0, 0, 100)  # 実数の場合はxはすべて0
+    y_values = np.linspace(y_min, y_max, 100)
 
 # y = x^n の計算
-y_values_complex = x_values_complex ** n
+z_values = (x_values + 1j * y_values) ** n
 
 # 3Dプロットの作成
 fig = go.Figure()
 
-# 複素数のxに対するプロット
-fig.add_trace(go.Surface(
-    x=x_values_complex.real,
-    y=x_values_complex.imag,
-    z=y_values_complex.real,
-    colorscale='Viridis',
-    cmin=y_min**n,
-    cmax=y_max**n
-))
+# 実数または純虚数のxに対するプロット
+if x_type == "Real":
+    fig.add_trace(go.Scatter3d(
+        x=x_values,
+        y=y_values,
+        z=z_values.real,
+        mode='lines',
+        line=dict(color='blue', width=3),
+        name=f'y = x^{n} (Real)'
+    ))
+else:
+    fig.add_trace(go.Scatter3d(
+        x=x_values,
+        y=y_values,
+        z=z_values.real,
+        mode='lines',
+        line=dict(color='red', width=3),
+        name=f'y = x^{n} (Imaginary)'
+    ))
 
 # プロットの設定
 fig.update_layout(
     scene=dict(
-        xaxis=dict(title='Real Part of X'),
-        yaxis=dict(title='Imaginary Part of X'),
-        zaxis=dict(title=f'Y = X^{n}'),
+        xaxis=dict(title='X'),
+        yaxis=dict(title='Y'),
+        zaxis=dict(title='Z'),
     ),
     title=f'3D Plot of y = x^{n}'
 )
