b6317fce553272923de1a0cca2e8be4a4f40b1de
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-11-09 06:38:56 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,30 @@
+import streamlit as st
+import plotly.graph_objects as go
+import numpy as np
+
+# Streamlitのページ設定
+st.set_page_config(page_title="Complex 3D Function Visualizer")
+
+# ヘッダー
+st.title("Complex 3D Function Visualizer")
+st.caption("Created by ChatGPT")
+
+# ユーザーからの入力を受け取る
+n = st.slider("Choose the exponent (n)", min_value=1, max_value=10, value=2)
+
+# 実数または純虚数のxの値を生成
+x_type = st.radio("Select x type", ["Real", "Imaginary"])
+if x_type == "Real":
+    x_min = st.number_input("Enter the minimum value of x (Real)", value=-10)
+    x_max = st.number_input("Enter the maximum value of x (Real)", value=10)
+    x_values = np.linspace(x_min, x_max, 100)
+    y_values = np.linspace(0, 0, 100)  # 純虚数の場合はyはすべて0
+else:
+    y_min = st.number_input("Enter the minimum value of y (Imaginary)", value=-10)
+    y_max = st.number_input("Enter the maximum value of y (Imaginary)", value=10)
+    x_values = np.linspace(0, 0, 100)  # 実数の場合はxはすべて0
+    y_values = np.linspace(y_min, y_max, 100)
+
 # y = x^n の計算
 z_values = (x_values + 1j * y_values) ** n
 
